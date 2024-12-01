ebd28fe0d3b3693e1e261c3d0434e15877060310
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-12-02 00:12:22 +0530

    updated site metadata

---
+++ src/config/site.js
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  shortName: "Calculator",
+  name: "Simple Calculator",
+  url: "https://itsancalculator.vercel.app",
+  ogImage:
+    "https://res.cloudinary.com/drzcgtzx8/image/upload/v1733078393/portfolio/calculator/wjkjoxuqjb7ch0zwjsyt.png",
+  description:
+    "Build clean future-rich websites using Notion as Database and CMS, handle websites content with ease using Notion, no more worry about database management and it's cost.",
+  links: {
+    gmail: "sujalvanjaredev@gmail.com",
+    github: "https://github.com/Sujal-Vanjare",
+    personalSite: "https://sujalvanjare.vercel.app",
+    twitter: "https://twitter.com/sujal169",
+    author: "Sujal",
+  },
+};
